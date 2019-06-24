005cafaee2f691b5a8f1b21be5afe679df88b86d
juan regonat <juanregonat@gmail.com>
2019-06-24 18:06:06 -0300

    Version con funcion y apertura de la carpeta final

--- PruebaArchivos4/Form1.cs
+++ PruebaArchivos4/Form1.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Diagnostics;
 
 namespace PruebaArchivos4
 {
     public partial class Form1 : Form
     {
-        string fileName1 = string.Empty;
-        string fileName2 = string.Empty;
         string sourcePath = string.Empty;
-        string targetPathNonDuplicated = string.Empty;
-        string targetPathDuplicated = string.Empty;
         private bool carpetaElegida = false;
 
         public Form1()
@@ -39,8 +31,6 @@ namespace PruebaArchivos4
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
                 sourcePath = folderBrowserDialog1.SelectedPath;
-                targetPathNonDuplicated = sourcePath + @"\archivosUnicos";
-                targetPathDuplicated = sourcePath + @"\archivosSospechosos";
                 carpetaElegida = true;
                 textBox1.Text = folderBrowserDialog1.SelectedPath;
             }
@@ -58,83 +48,10 @@ namespace PruebaArchivos4
                 return;
             }
 
-            // Use Path class to manipulate file and directory paths.
-            string sourceFile = Path.Combine(sourcePath, fileName1);
-            string destFileOriginal = string.Empty;
-            string destFileDuplicated = string.Empty;
-
-
             if (Directory.Exists(sourcePath))
             {
                 string[] files = Directory.GetFiles(sourcePath);
-
-                //COMPARADOR:
-                string[] miArray = files;
-                //Array.Sort(miArray);
-                Array.Reverse(miArray);
-                int i = 0;
-                int j = 0;
-
-                for (i = 0; i < miArray.Length; i++) //toma el primer archivo para compararlo
-                {
-                    for (j = i + 1; j < miArray.Length; j++) //toma el segundo archivo para compararlo
-                    {
-
-                        // Use static Path methods to extract only the file name from the path.
-                        fileName1 = Path.GetFileName(miArray[i]);
-                        fileName2 = Path.GetFileName(miArray[j]);
-
-
-                        int largo = fileName2.Length;              //22
-                        int inicio = fileName2.IndexOf("cop", StringComparison.CurrentCultureIgnoreCase) - 3; // donde comienza: 9
-                        int fin = fileName2.IndexOf(".");          // 18, cuantos recorto?: archivo 1 - Copiar.txt largo - (inicio + fin)
-
-                        if (inicio > 0)
-                        {   
-                            string nombreTemp = fileName2.Remove(inicio, fin - inicio);
-                            if (fileName1 == nombreTemp)
-                            {
-                                //resultado = "valor duplicado" : entonces creo una carpeta, y muevo al archivo 2 para ahi;
-                                Directory.CreateDirectory(targetPathDuplicated);
-                                destFileDuplicated = Path.Combine(targetPathDuplicated, fileName2);
-                                File.Move(miArray[j], destFileDuplicated);
-                            }
-                        }
-
-
-                        //tomo el segundo nombre y le busco parentesis
-                        int foundParenthesis1 = fileName2.IndexOf("(");
-                        int foundParenthesis2 = fileName2.IndexOf(")");
-                        if (foundParenthesis1 != foundParenthesis2 && foundParenthesis1 >= 0)
-
-                        {   //usar un nombre temporal para ver si es igual a fileName1 o no
-                            string fileName2Temp = (fileName2.Remove(foundParenthesis1 - 9, foundParenthesis2 - foundParenthesis1 + 1)).Trim();
-                            if (fileName1 == fileName2Temp)                            
-                            {
-                                //resultado = "valor duplicado" : entonces creo una carpeta, y muevo al archivo 2 para ahi;
-                                Directory.CreateDirectory(targetPathDuplicated);
-                                destFileDuplicated = Path.Combine(targetPathDuplicated, fileName2);
-                                File.Move(miArray[j], destFileDuplicated);
-                            }
-                        }
-
-                    } //termina la comparación del segundo archivo y va a tomar otro.
-
-
-                    try
-                    {
-                        //resultado = "valor único". Ya comparamos el archivo 1 con todos los demás y ahora lo ponemos en "originales" y tomamos otro archivo.
-                        Directory.CreateDirectory(targetPathNonDuplicated);
-                        destFileOriginal = Path.Combine(targetPathNonDuplicated, fileName1);
-                        File.Move(miArray[i], destFileOriginal);
-                    } catch (Exception ex)
-                    {
-
-                    }
-
-                } //termina la comparacion del primer archivo y va a tomar otro.
-
-
+                buscarDuplicados(files);
             }
             else
             {
@@ -142,8 +59,84 @@ namespace PruebaArchivos4
             }
 
             MessageBox.Show("Terminé");
+            Process.Start("explorer.exe", sourcePath);
             Application.Exit();
             
         }
+
+        private void buscarDuplicados(string[] array) {
+            string[] miArray = array;
+            Array.Reverse(miArray);
+            int i = 0;
+            int j = 0;
+            string fileName1 = string.Empty;
+            string fileName2 = string.Empty;
+
+            // Use Path class to manipulate file and directory paths.
+            string sourceFile = Path.Combine(sourcePath, fileName1);
+            string destFileOriginal = string.Empty;
+            string destFileDuplicated = string.Empty;
+
+            string targetPathNonDuplicated = sourcePath + @"\archivosUnicos";
+            string targetPathDuplicated = sourcePath + @"\archivosSospechosos";
+            
+            for (i = 0; i < miArray.Length; i++) //toma el primer archivo para compararlo
+            {
+                for (j = i + 1; j < miArray.Length; j++) //toma el segundo archivo para compararlo
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    fileName1 = Path.GetFileName(miArray[i]);
+                    fileName2 = Path.GetFileName(miArray[j]);
+
+                    int largo = fileName2.Length;
+                    int inicio = fileName2.IndexOf("cop", StringComparison.CurrentCultureIgnoreCase) - 3;
+                    int fin = fileName2.IndexOf(".");
+
+                    if (inicio > 0)
+                    {
+                        string nombreTemp = fileName2.Remove(inicio, fin - inicio);
+                        if (fileName1 == nombreTemp)
+                        {
+                            //resultado = "valor duplicado" : entonces creo una carpeta, y muevo al archivo 2 para ahi;
+                            Directory.CreateDirectory(targetPathDuplicated);
+                            destFileDuplicated = Path.Combine(targetPathDuplicated, fileName2);
+                            File.Move(miArray[j], destFileDuplicated);
+                        }
+                    }
+
+                    //tomo el segundo nombre y le busco parentesis
+                    int foundParenthesis1 = fileName2.IndexOf("(");
+                    int foundParenthesis2 = fileName2.IndexOf(")");
+                    if (foundParenthesis1 != foundParenthesis2 && foundParenthesis1 >= 0)
+
+                    {   //usar un nombre temporal para ver si es igual a fileName1 o no
+                        string fileName2Temp = (fileName2.Remove(foundParenthesis1 - 9, foundParenthesis2 - foundParenthesis1 + 1)).Trim();
+                        if (fileName1 == fileName2Temp)
+                        {
+                            //resultado = "valor duplicado" : entonces creo una carpeta, y muevo al archivo 2 para ahi;
+                            Directory.CreateDirectory(targetPathDuplicated);
+                            destFileDuplicated = Path.Combine(targetPathDuplicated, fileName2);
+                            File.Move(miArray[j], destFileDuplicated);
+                        }
+                    }
+
+                } //termina la comparación del segundo archivo y va a tomar otro.
+
+
+                try
+                {
+                    Directory.CreateDirectory(targetPathNonDuplicated);
+                    destFileOriginal = Path.Combine(targetPathNonDuplicated, fileName1);
+                    File.Move(miArray[i], destFileOriginal);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            } //termina la comparacion del primer archivo y va a tomar otro.
+
+        }        
+
     }
 }
